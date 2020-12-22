9db2f2ff182aee5873d5f0dc56731d4b32b9e7a1
raulzc3 <raulzc3pc@gmail.com>
2020-12-22 01:50:30 +0100

    Edición de campos

--- db/creacionBaseDatos.sql
+++ db/creacionBaseDatos.sql
@@ -1,5 +1,6 @@
-/* Script de creación de la base de datos para el proyecto "GESTIÓN DE COWORKING" creado por:
-    Julián David Calle Cristamcho
+/* 
+Script de creación de la base de datos para el proyecto "GESTIÓN DE COWORKING" creado por:
+    Julián David Calle Cristancho
     Raúl González Seco
     Iago Ubeira Martínez
 */
@@ -9,34 +10,34 @@ CREATE DATABASE IF NOT EXISTS coworking CHARSET "utf8mb4" COLLATE "utf8mb4_spani
 USE coworking;
 
 CREATE TABLE usuarios (
-    ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
-    clave VARCHAR(30) NOT NULL, -- Contraseña del usuario
+    nif CHAR(9) NOT NULL UNIQUE,
+    clave VARCHAR(50) NOT NULL, -- Contraseña del usuario (cifrada)
     foto VARCHAR(50) DEFAULT '/img/default.png', -- En caso de que un usuario no suba una foto, le asignaremos una por defecto
-    biografia VARCHAR(280),
     email VARCHAR(100) NOT NULL UNIQUE, -- UNIQUE para que no se pueda crear más de un usuario con el mismo correo
     tlf VARCHAR(30) NOT NULL UNIQUE, -- UNIQUE para que no se pueda crear más de un usuario con el mismo teléfono
     administrador TINYINT DEFAULT 0, -- Por defecto, los usuarios no serán administradores
+    verificado TINYINT DEFAULT 0, -- Por defecto, un usuario no estará verificado
     fecha_nacimiento DATE NOT NULL,
     empresa VARCHAR(50), -- Empresa en la que trabaja el usuario
-    CONSTRAINT usuarios_administrador_ck1 CHECK (administrador = 1 OR administrador = 0) -- Hacemos que el campo administrador se asemeje a un booleano
+    CONSTRAINT usuarios_administrador_ck1 CHECK (administrador = 1 OR administrador = 0), -- Hacemos que el campo administrador se asemeje a un booleano
+	CONSTRAINT usuarios_verificado_ck2 CHECK (verificado = 1 OR verificado = 0) -- Hacemos que el campo verificado se asemeje a un booleano
 );
 
 CREATE TABLE espacios (
     ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	tipo ENUM("Sala de reuniones", "Oficina individual", "Auditorio", "Sala audiovisual", "Oficina compartida") NOT NULL, -- Solo existirán los tipos de espacios aquí definidos
     nombre VARCHAR(50) NOT NULL,
-    hora_apertura TIME NOT NULL,
-    hora_cierre TIME NOT NULL,
     precio SMALLINT UNSIGNED NOT NULL,
     aforo SMALLINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE fotos (
 	ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    url VARCHAR(50) UNIQUE NOT NULL, -- URL de la imagen que queramos usar
-	id_espacio INT UNSIGNED, 
+    url VARCHAR(50) UNIQUE NOT NULL, -- URL de la imagen
+	id_espacio INT UNSIGNED,  
     CONSTRAINT espacios_fotos_fk1 FOREIGN KEY (id_espacio)
 		REFERENCES espacios (ID)
 );
@@ -44,17 +45,18 @@ CREATE TABLE fotos (
 CREATE TABLE packs ( -- Los packs serán conjuntos preestablecidos de servicios que se incluirán en el pedido
     ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     tipo ENUM('Básico', 'Intermedio', 'Audiovisual', 'Informático') NOT NULL, -- Algunos de los tipos de packs que existirán, se añadirán más durante el desarrollo del proyecto 
-    texto_contenido TEXT NOT NULL, -- Todos los servicios que incluirá cada pack (mayor velocidad de conexión, proyector, cafetería...)
+    texto_contenido TEXT NOT NULL, -- Todos los servicios que incluirá cada pack (mayor velocidad de conexión, proyector, acceso a cafetería...)
     precio SMALLINT UNSIGNED NOT NULL,
-    foto VARCHAR(50) NOT NULL -- URL de la imagen que queramos usar
+    foto VARCHAR(50) NOT NULL -- URL de la imagen
 );
 
 CREATE TABLE valoraciones (
     ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     comentario VARCHAR(500) NOT NULL, 
     calificacion TINYINT UNSIGNED DEFAULT 5, 
+    fecha_valoracion DATE NOT NULL, -- **************************** REVISAR CONSTRAINT ************
     id_usuario INT UNSIGNED NOT NULL, 
-    id_espacio INT UNSIGNED NOT NULL, 
+    id_espacio INT UNSIGNED NOT NULL,     
     CONSTRAINT valoraciones_usuarios_fk1 FOREIGN KEY (id_usuario) 
         REFERENCES usuarios (ID), 
     CONSTRAINT valoraciones_espacios_fk2 FOREIGN KEY (id_espacio) 
@@ -84,8 +86,6 @@ CREATE TABLE pedidos (
     fecha_inicio DATE NOT NULL, -- Fecha de inicio del periodo reservado
     fecha_fin DATE NOT NULL,	-- Fecha final del periodo reservado
     precio_pedido SMALLINT NOT NULL, -- Precio total del pedido (precio pack + precio espacio)
-    hora_uso_inicio TIME NOT NULL, -- Hora a la que tendrán acceso al espacio reservado cada uno de los días (todos a la misma hora) dentro del periodo reservado
-    hora_uso_fin TIME NOT NULL, -- Hora a la que dejarán de tener acceso al espacio reservado cada uno de los días (todos a la misma hora) dentro del periodo reservado
     id_usuario INT UNSIGNED NOT NULL,
     id_espacio INT UNSIGNED NOT NULL,
     id_pack INT UNSIGNED NOT NULL,
@@ -95,7 +95,11 @@ CREATE TABLE pedidos (
         REFERENCES espacios (ID),
     CONSTRAINT pedidos_packs_fk3 FOREIGN KEY (id_pack)
         REFERENCES packs (ID),
+    CONSTRAINT espacios_fecha_inicio_uq1 UNIQUE (id_espacio, fecha_inicio), -- Nos aseguramos que no se puede reservar el mismo espacio en la misma fecha más de una vez
+    CONSTRAINT espacios_fecha_fin_uq2 UNIQUE (id_espacio, fecha_fin),  -- Nos aseguramos que no se puede reservar el mismo espacio en la misma fecha más de una vez
     CONSTRAINT pedididos_fechas_ck1 CHECK(fecha_inicio <= fecha_fin AND fecha_pedido <= fecha_inicio) /* Nos aseguramos de que la fecha de inicio de la reserva es anterior 
-                                                                                                        a la fecha de finalización de la misma y de que no se hace una reserva que se inicie
-                                                                                                        antes del día en el que se realiza el pedido */    
-);
+																										antes del día en el que se realiza el pedido */    
+	);
+    
+    
+    

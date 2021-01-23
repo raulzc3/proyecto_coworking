bc3c95faf7c25b750127a72b4e3bdf9817da4f16
raulzc3 <raulzc3pc@gmail.com>
2021-01-23 18:08:56 +0100

    Solucionamos error del usuario que puede eliminar cualquier reseña

--- controllers/reviews/deleteReview.js
+++ controllers/reviews/deleteReview.js
@@ -1,13 +1,26 @@
+const { createError } = require("../../helpers");
+
 const deleteReview = async (req, res, next) => {
   let connection;
   try {
     connection = await req.app.locals.getDB();
-    const {  review_id } = req.params;
+    const { review_id, user_id } = req.params;
 
-    await connection.query(
-      `DELETE FROM reviews WHERE ID= ?;`,
-      [ review_id]
+    const [reviewUser] = await connection.query(
+      `
+SELECT user_id 
+FROM reviews
+WHERE id = ?
+`,
+      review_id
     );
+
+    //Comprobamos que un usuario solo sea capaz de eliminar sus propias reseñas (el admin puede borrar las que quiera)
+    if (Number(user_id) !== reviewUser && !req.userAuth.admin) {
+      throw createError("No estás autorizado para realizar esta acción", 401);
+    }
+
+    await connection.query(`DELETE FROM reviews WHERE ID= ?;`, [review_id]);
 
     res.send({
       stats: "ok",

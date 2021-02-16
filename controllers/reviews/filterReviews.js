e9ca530ef611a26b8e11c6e4f80aa9705bcd99b5
raulzc3 <raulzc3pc@gmail.com>
2021-02-17 00:00:49 +0100

    Editada query defilterReviews

--- controllers/reviews/filterReviews.js
+++ controllers/reviews/filterReviews.js
@@ -11,24 +11,22 @@ const filterReviews = async (req, res, next) => {
     //Filtrar por id/usuario/tipo de espacio/fecha
     const {
       review_id,
-      user_id,
       review_date,
       order,
       direction,
-      space_id,
       user_name,
       space_name,
     } = req.query;
 
     review_date ? new Date(review_date) : new Date(1111 - 11 - 11);
-    const orderBy = order ? order : `score`;
+    const orderBy = order ? order : `r.id`;
     const orderDirection = direction ? direction : `ASC`;
     const [results] = await connection.query(
       `
-  SELECT r.id as "id", CONCAT(u.name, " ", surname) "user_name", comment, score, review_date, user_id, space_id,s.name
+  SELECT r.id as "id", CONCAT(u.name, " ", surname," (", s.id, ")" ) "user_name", comment, score, review_date, user_id, CONCAT(s.name, " (", s.id, ")" ) "space_name"
   FROM reviews r JOIN users u ON r.user_id = u.id JOIN spaces s ON r.space_id=s.id
-  WHERE (r.ID = ? OR ?) AND (CONCAT(u.name, " ", surname) LIKE ? OR ?) AND (user_id =? OR ?) AND (space_id =? OR ?) 
-  AND (DATE(review_date)=DATE(?) OR ?) AND (s.name LIKE ? OR ?)
+  WHERE (r.ID = ? OR ?) AND (CONCAT(u.name, " ", surname," (", s.id, ")" ) LIKE ? OR ?)
+  AND (DATE(review_date)=DATE(?) OR ?) AND (CONCAT(s.name, " (", s.id, ")" ) LIKE ? OR ?)
   ORDER BY ${orderBy} ${orderDirection};
   `,
       [
@@ -36,10 +34,6 @@ const filterReviews = async (req, res, next) => {
         !review_id,
         `%${user_name}%`,
         !user_name,
-        user_id,
-        !user_id,
-        space_id,
-        !space_id,
         review_date,
         !review_date,
         `%${space_name}%`,

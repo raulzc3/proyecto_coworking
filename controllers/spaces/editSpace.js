d22d37301c4ef63f253ee46895622ee608fb3b44
vira <ubeiram@gmail.com>
2021-02-23 02:31:41 +0100

    edit spaces acepta foto y las devuelve en su respuesta

--- controllers/spaces/editSpace.js
+++ controllers/spaces/editSpace.js
@@ -1,5 +1,5 @@
 const { newSpaceSchema } = require("../../schemas");
-const { validator } = require("../../helpers");
+const { validator, savePhoto, setPhotoUrl } = require("../../helpers");
 
 const editSpace = async (req, res, next) => {
   let connection;
@@ -13,6 +13,16 @@ const editSpace = async (req, res, next) => {
 
     const { type, description, name, price, capacity } = req.body;
 
+    //Compruebo si se envió foto y si es correcta
+    if (!req.files) {
+      throw createError("Tienes que añadir una foto ", 400);
+    }
+    if (Object.keys(req.files).length > 20) {
+      throw createError("Solo se permite subir 20 fotos", 400);
+    }
+    //Lo convierto en array para hacer un map (hay varias fotos)
+    const keysOfPhotos = await Object.entries(req.files);
+
     //hago un query de SQL Update para editar los datos
     await connection.query(
       `
@@ -20,6 +30,25 @@ const editSpace = async (req, res, next) => {
     WHERE ID=?
     ;`,
       [type, description, name, price, capacity, space_id]
+    );
+
+    //Selecciono la subcarpeta para almacenar las fotos del espacio
+    const folderOfPhotos = `spaces/${space_id}`;
+
+    const photosData = keysOfPhotos.map((value) => {
+      return value[1];
+    });
+    const photos = await Promise.all(
+      photosData.map(async (photo) => {
+        const namePhoto = await savePhoto(photo, folderOfPhotos);
+        //Introduzco las fotos en la BD
+        await connection.query(
+          `INSERT INTO photos (url,space_id)
+  VALUES ( ?, ?);`,
+          [namePhoto, space_id]
+        );
+        return setPhotoUrl(namePhoto, folderOfPhotos);
+      })
     );
 
     res.send({
@@ -31,6 +60,7 @@ const editSpace = async (req, res, next) => {
         name,
         price,
         capacity,
+        photos: [...photos],
       },
     });
   } catch (error) {

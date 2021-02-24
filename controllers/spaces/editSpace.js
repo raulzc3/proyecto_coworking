4c2ab8558c6f72b19e42a78142355ef4e501fd77
Julian-Calle <jcallecristancho@gmail.com>
2021-02-24 01:53:21 +0100

    edit space fixed

--- controllers/spaces/editSpace.js
+++ controllers/spaces/editSpace.js
@@ -18,40 +18,45 @@ const editSpace = async (req, res, next) => {
 
     const { type, description, name, price, capacity } = req.body;
 
-    //Compruebo  si es correcta
-    if (Object.keys(req.files).length > 20) {
-      throw createError("Solo se permite subir 20 fotos", 400);
-    }
-    //Lo convierto en array para hacer un map (hay varias fotos)
-    const keysOfPhotos = await Object.entries(req.files);
-
     //hago un query de SQL Update para editar los datos
     await connection.query(
       `
-    UPDATE spaces SET type=?,description=?,name=?,price=?,capacity=?
-    WHERE ID=?
-    ;`,
+      UPDATE spaces SET type=?,description=?,name=?,price=?,capacity=?
+      WHERE ID=?
+      ;`,
       [type, description, name, price, capacity, space_id]
     );
 
-    //Selecciono la subcarpeta para almacenar las fotos del espacio
-    const folderOfPhotos = `spaces/${space_id}`;
+    let listOfphotos;
+    if (req.files) {
+      //Compruebo  si es correcta
+      if (Object.keys(req.files).length > 20) {
+        throw createError("Solo se permite subir 20 fotos", 400);
+      }
+      //Lo convierto en array para hacer un map (hay varias fotos)
+      const keysOfPhotos = await Object.entries(req.files);
+      //Selecciono la subcarpeta para almacenar las fotos del espacio
+      const folderOfPhotos = `spaces/${space_id}`;
 
-    const photosData = keysOfPhotos.map((value) => {
-      return value[1];
-    });
-    const photos = await Promise.all(
-      photosData.map(async (photo) => {
-        const namePhoto = await savePhoto(photo, folderOfPhotos);
-        //Introduzco las fotos en la BD
-        await connection.query(
-          `INSERT INTO photos (url,space_id)
+      const photosData = keysOfPhotos.map((value) => {
+        return value[1];
+      });
+      listOfphotos = await Promise.all(
+        photosData.map(async (photo) => {
+          const namePhoto = await savePhoto(photo, folderOfPhotos);
+          //Introduzco las fotos en la BD
+          await connection.query(
+            `INSERT INTO photos (url,space_id)
   VALUES ( ?, ?);`,
-          [namePhoto, space_id]
-        );
-        return setPhotoUrl(namePhoto, folderOfPhotos);
-      })
-    );
+            [namePhoto, space_id]
+          );
+          return setPhotoUrl(namePhoto, folderOfPhotos);
+        })
+      );
+    }
+    const photos = req.files
+      ? [...listOfphotos]
+      : "No se ha añadido ninguna foto";
 
     res.send({
       status: "ok",
@@ -62,7 +67,7 @@ const editSpace = async (req, res, next) => {
         name,
         price,
         capacity,
-        photos: [...photos],
+        photos,
       },
     });
   } catch (error) {
